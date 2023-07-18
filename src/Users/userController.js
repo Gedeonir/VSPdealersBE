df77cb86bf1fb0f5d5e1686a64b1623235475527
Gedeonir <irafasha.jedy12@gmail.com>
2023-07-18 12:43:48 +0200

    EmailFix

--- src/Users/userController.js
+++ src/Users/userController.js
@@ -1,15 +1,16 @@
 const {User}=require('../../models/users')
-const emailValidator = require('deep-email-validator');
+// const emailValidator = require('deep-email-validator');
 const sendEmail = require("../utils/Email");
 const bcrypt = require("bcryptjs");
 const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
+const validator = require('validator');
 
 dotenv.config();
 
 async function isEmailValid(email) {
-    return await emailValidator.validate(email)
-  }
+    return  validator.isEmail(email)
+}
 
 
 const RegisterUser=async(req,res)=>{
@@ -29,17 +30,11 @@ const RegisterUser=async(req,res)=>{
             })
         }
 
-        const {valid, reason, validators} = await isEmailValid(email);
 
 
-        if(!valid){
+        if(!isEmailValid(email)){
             return res.status(409).json({
-                message:"Please provide valid email",
-                data:{
-                    valid,
-                    reason,
-                    validators
-                }
+                message:"Please provide valid email"
             })
         }
 
@@ -66,7 +61,7 @@ const RegisterUser=async(req,res)=>{
         });
 
 
-        const URL = `<div style="width:50%;margin-left:auto;margin-right:auto"><a href='${process.env.BACKEND_URL}/api/v1/users/verifyAccount/${newUser._id}' style='padding:10px 30px;color:#fff;text-decoration:none; background-color:#ef4444; font-weight:700; border-radius:10px;'>Verify account</a></div>`;
+        const URL = `<div style="width:50%;margin-left:auto;margin-right:auto"><a href="${process.env.BACKEND_URL}/api/v1/users/verifyAccount/${newUser._id}" style='padding:10px 30px;color:#fff;text-decoration:none; background-color:#ef4444; font-weight:700; border-radius:10px;'>Verify account</a></div>`;
         const message = `
         <div>
             <div style="font-weight:700"><h1>Account verification</h1></div>
